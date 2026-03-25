61859d87cca6c8064439d3819a0d6b55c405d8dc
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2026-03-25 15:55:44 +0100

    feat(onboarding): add onboarding dialog and localization
    * Implement onboarding dialog component with steps for new users
    * Add localization for onboarding titles and descriptions in English, Spanish, French, and Italian
    * Introduce OnboardingCompletedAt field in user model to track onboarding completion
    * Create migration to add OnboardingCompletedAt column to AspNetUsers table

---
+++ LearnStack/Data/Migrations/20260325145316_AddOnboardingCompletedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnStack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOnboardingCompletedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OnboardingCompletedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OnboardingCompletedAt",
+                table: "AspNetUsers");
+        }
+    }
+}
